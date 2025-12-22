a5ea880e574b9e631207a6b4686c71bdb9f8f12b
WaelAlSyaidKhalil <waell.ite.sy@gmail.com>
2025-12-23 02:53:36 +0300

    edit lexer & parser
    Co-Authored-By: Yousef Alsati <190262878+alsati21@users.noreply.github.com>
    Co-Authored-By: waleed_Alsati <alsatywlyd54@gmail.com>

--- src/antlr/gen/ProductLexer.java
+++ src/antlr/gen/ProductLexer.java
@@ -1,5 +1,4 @@
 // Generated from C:/FlaskTemplateCompiler/src/antlr/ProductLexer.g4 by ANTLR 4.13.2
-package antlr;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
